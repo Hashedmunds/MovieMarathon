85d160c9c6bc9cb23407b1baad405034fa633dc5
Hash Edmundson <hedmunds@usc.edu>
2020-02-01 16:06:10 -0800

    Populdate dropdown values

--- scripts.js
+++ scripts.js
@@ -1,17 +1,38 @@
+var api_key = '43f9f3c9828f30fa5e2f6f6a7b11b83e';
+
+
 $(document).ready(function() {
+
   // initial setup
   // $('form').css('display', 'none');
 
   // possible selections of types of marathons
   // TODO: decide whether you want to keep or delete
-  // let typeMarathon = ['Genre', 'Actor', 'Year Released', 'Foreign Top Charts'];
-  // let $typeMarathonDropdown = $('.firstMenu select');
-  // $.each(typeMarathon, function() {
-  //   let $option = $('<option></option>');
-  //   $option.val(this);
-  //   $typeMarathonDropdown.append($option);
-  //
-  // })
+  let typeMarathon = ['Genre', 'Actor', 'Year Released', 'Foreign Top Charts'];
+  let $typeMarathonDropdown = $('.firstMenu select');
+  $.each(typeMarathon, function() {
+    let $option = $('<option></option>');
+    $option.val(this);
+    $option.text(this);
+    $typeMarathonDropdown.append($option);
+  })
+
+  //Populate Genres
+  fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + api_key + '&language=en-US')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    var genres = data.genres;
+    let $typeGenreDropdown = $('.secondMenu select');
+    for(i in data.genres){
+      let $option = $('<option></option>');
+      $option.val(genres[i].name);
+      $option.text(genres[i].name);
+      $typeGenreDropdown.append($option);
+    }
+  })
+
 
   // populates the options for user to select
   // TODO: put all the names of the genres
